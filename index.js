addff3c8f3bbb0d1d0de0534108e557892e1c147
wptasmina <wptasmina@gmail.com>
2025-01-14 01:21:03 +0600

    Added wishList

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 const port = process.env.PORT || 5000
@@ -29,14 +29,7 @@ async function run() {
 
     const blogUserCollection = client.db("blog-discover").collection("blogger");
 
-
-    //add blogs
-    app.post('/blogger', async(req, res)=>{
-      const newAddBlog = req.body;
-      const result = await blogUserCollection.insertOne(newAddBlog)
-      res.status(201).send(result);
-    });
-
+    //add blogs get
     app.get('/blog', async(req, res)=>{
       const result = await blogUserCollection.find().sort({_id: -1}).toArray();
       // const result = await blogUserCollection.find().toArray();
@@ -44,10 +37,155 @@ async function run() {
     })
 
     app.get('/homeblog', async(req, res)=>{
-      const result = await blogUserCollection.find().limit(6).toArray();
+      const result = await blogUserCollection.find().limit(6).sort({_id: -1}).toArray();
       res.send(result)
     })
+
+  //add blogs post
+  app.post('/blogger', async(req, res)=>{
+    const newAddBlog = req.body;
+    const result = await blogUserCollection.insertOne(newAddBlog)
+    res.status(201).send(result);
+  });
+
+
+  // Delete
+  app.delete("/deleteBlog/:id",async (req, res)=> {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await blogUserCollection.deleteOne(query)
+    res.send(result)
+  })
+
+
+// Get  details blog
+app.get('/details/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await blogUserCollection.findOne(query)
+  res.send(result)
+  });
+  
+
+
+// Add comment
+app.post('/comments', async (req, res) => {
+  try {
+    const { blogId, userName, text } = req.body;
+
+    if (!blogId || !userName || !text) {
+      return res.status(400).json({ message: 'Missing required fields' });
+    }
+
+    const comment = new Comment(req.body);
+    await comment.save();
+    res.status(201).json(comment);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error adding comment' });
+  }
+});
+
+// Get comments for a blog
+// app.get('/comments/:blogId', async (req, res) => {
+//   try {
+//     const { blogId } = req.params;
+//     if (!isValidObjectId(blogId)) {
+//       return res.status(400).json({ message: 'Invalid Blog ID' });
+//     }
+
+//     const comments = await Comment.find({ blogId });
+//     res.json(comments);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Error fetching comments' });
+//   }
+// });
+
+    // const Blog = require('../models/Blog');
+    // const Comment = require('../models/Comment');
+    
+    // // Get blog details
+    // router.get('/details/:id', async (req, res) => {
+    //   const blog = await Blog.findById(req.params.id);
+    //   res.json(blog);
+    // });
+    
+    // // Add comment
+    // router.post('/comments', async (req, res) => {
+    //   const comment = new Comment(req.body);
+    //   await comment.save();
+    //   res.json(comment);
+    // });
+    
+    // // // Get comments for a blog
+    // router.get('/comments/:blogId', async (req, res) => {
+    //   const comments = await Comment.find({ blogId: req.params.blogId });
+    //   res.json(comments);
+    // });
+    
+
+     // update page
+    app.get('/blog/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await blogUserCollection.findOne(query)
+      res.send(result)
+      console.log(result)
+
+    });
+
+    app.put('/blog/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updateBlog = req.body;
+      const updates = {
+        $set: {
+          userName: updateBlog.userName, 
+          email: updateBlog.email, 
+          userImage: updateBlog.userImage, 
+          title: updateBlog.title, 
+          imageUrl: updateBlog.imageUrl, 
+          date: updateBlog.date, 
+          shortDesc: updateBlog.shortDesc, 
+          longDesc: updateBlog.longDesc, 
+          category: updateBlog.category
+        }
+      }
+
+      const result = await blogUserCollection.updateOne(filter, updates, options )
+      res.send(result)
+    })
+// update page End
+
+    //wishlist page
+    app.get('/wishlist/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await blogUserCollection.findOne(query)
+      res.send(result)
+      console.log(result)
+
+    })
+    app.get('/wishlist', async(req, res)=>{
+      const result = await blogUserCollection.find().toArray();
+      res.send(result)
+
+    })
+
+    app.post('/wishlist', async (req, res) => {
+      const newAddBlog = req.body;
+      const result = await blogUserCollection.insertOne(newAddBlog)
+      res.status(201).send(result);
+    });
+ //wishlist page End
  
+  
+
+
+
+
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
